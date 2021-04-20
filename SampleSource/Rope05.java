7b81b2396cedac781f360150c0f22b2184fd957c
masatokg <masatokg@users.noreply.github.com>
2021-04-20 14:49:34 +0900

    Create Rope05.java
    ファイルを修正してプログラムを完成させてください
    ※縄跳びを５回飛ぶたび、出力メッセージの回数の部分をカウントアップさせる

---
+++ SampleSource/Rope05.java
@@ -0,0 +1,9 @@
+public class Rope05 {
+    public static void main(String args[]) {
+        // カウンター変数は「cnt」とし、0で初期化する
+        // 1回目は、「1回目！」と出力する
+
+        // 出力メッセージの例
+        System.out.printf("%d回目！", (cnt+1));
+    }
+}
