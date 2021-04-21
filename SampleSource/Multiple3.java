d7f03a3fa086db61c7f926bb37fbb423fe4837f0
masatokg <masatokg@users.noreply.github.com>
2021-04-22 02:08:57 +0900

    Create Multiple3.java

---
+++ SampleSource/Multiple3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Multiple3 {
+    public static void main(String args[]) {
+        // 入力された正の整数の0以上の倍数を数える
+        Scanner scanner = new Scanner(System.in);
+        int cnt=0, max=scanner.nextInt();
+
+        
+        System.out.printf("%d個めの3の倍数は%d\n", ++cnt, i);
+
+    
+        System.out.printf("3の倍数は%d個ありました\n", cnt);
+    }
+}
