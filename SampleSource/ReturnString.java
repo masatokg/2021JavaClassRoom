4cb3043db5f5ebd651dba3db3ff7d3f8de76a29e
masatokg <masatokg@users.noreply.github.com>
2021-05-26 02:00:50 +0900

    Create ReturnString.java

---
+++ SampleSource/ReturnString.java
@@ -0,0 +1,13 @@
+public class ReturnString {
+    public static void main(String[] args){
+        // String型戻り値のメソッドを呼び出し、戻り値を変数msgに代入
+        ??????????????????
+
+        // 結果を表示
+        System.out.printf("戻り値は「%s」です\n", msg);
+    }
+    // 文字列を返すメソッド
+    public static String returnString(){
+        return "this is return value!!";
+    }
+}
