2837fe7759843744fd1575ec1d66e89ca462838e
masatokg <masatokg@users.noreply.github.com>
2021-05-26 01:56:08 +0900

    Create MyParam.java

---
+++ SampleSource/MyParam.java
@@ -0,0 +1,14 @@
+public class MyParam {
+    public static void main(String[] args){
+        // メソッドを呼び出して実引数を引き渡す
+        String rt = ??????(5);
+        // 結果を表示
+        System.out.println(????);
+    }
+    // 仮引数numを10倍して組み立てた文字列の結果を返すメソッド
+    public static String myParam(?????? num){
+        // String.formatメソッドで文字列を組み立てる
+        String str = String.format("%dの10倍は%dです", num, (num*10));
+        return str;
+    }
+}
