808d139badcc9be863e03cc885579936087bf600
masatokg <masatokg@users.noreply.github.com>
2021-05-26 03:35:24 +0900

    Create SignatureSample2.java

---
+++ SampleSource/SignatureSample2.java
@@ -0,0 +1,20 @@
+public class SignatureSample2 {
+    public static void main(String[] args){
+
+        // メソッドをシグニチャで呼び出す
+        ???????????????????
+
+    }
+    public static void signatureSample2(){
+        System.out.println("Hello");
+    }
+    public static void signatureSample2(int param){
+        System.out.printf("Hello %d\n", param);
+    }
+    public static void signatureSample2(String param){
+        System.out.printf("Hello %s\n", param);
+    }
+    public static void signatureSample2(int param1, int param2){
+        System.out.printf("Hello %d, %d\n", param1, param2);
+    }
+}
