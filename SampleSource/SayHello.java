b7d2ef56d122d0e9b77c6e1666fb4750dcbdb7b2
masatokg <masatokg@users.noreply.github.com>
2021-05-26 02:02:35 +0900

    Create SayHello.java

---
+++ SampleSource/SayHello.java
@@ -0,0 +1,11 @@
+public class SayHello {
+    public static void main(String[] args){
+        // メソッドを呼び出す
+        sayHello();
+    }
+    // メッセージを出力するメソッド
+    public static void ??????(){
+        // メッセージを画面に出力
+        System.out.println("sayHello");
+    }
+}
